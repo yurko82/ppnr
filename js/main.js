0d996f0b1b1b3933c234ef370bc8f516319bff29
yurko82 <yurko1982@gmail.com>
2020-10-05 12:20:22 +0300

    btn up

--- js/main.js
+++ js/main.js
@@ -237,4 +237,19 @@
     }
     checkNewsType();
 
+    //btn up
+    $('body').append('<button class="btn_up" />');
+    $('.btn_up').click(function(){
+        $('body').animate({'scrollTop': 0}, 1000);
+        $('html').animate({'scrollTop': 0}, 1000);
+    });
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > 300) {
+            $('.btn_up').addClass('active');
+        }
+        else{
+            $('.btn_up').removeClass('active');
+        }
+    });
+
 })();
