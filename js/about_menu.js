f414aa3f161ef68e6b0ba306b2628e6f002d548d
spineag <spineag@gmail.com>
2020-12-17 20:16:52 +0200

    about_menu v2 -part2

--- js/about_menu.js
+++ js/about_menu.js
@@ -1,5 +1,19 @@
-$('.about_side_menu-select>span').text($('.about_side_menu-submenu li.active').text());
+$('.about_side_menu .about_side_menu-select>span').text($('.about_side_menu .about_side_menu-submenu li.active').text());
+$('.about_side_menu ul.active').click(()=>openSideMenu(false));
+$(document).click((e)=>{ 
+    if (!$(e.target).parents('.about_side_menu-submenu').length && !$(e.target).hasClass('about_side_menu-submenu')
+        && !$(e.target).hasClass('about_side_menu-select') && !$(e.target).parents('.about_side_menu-select').length) 
+        openSideMenu(false); 
+});
 
-function openSubmenu(el){
-    console.log(el);
+function openSideMenu(open=true){
+    if (open) {
+        $('.about_side_menu ul').addClass('open');
+        $('.about_side_menu-select').css('display','none');
+    } else {
+        $('.about_side_menu ul').removeClass('open');
+        $('.about_side_menu-select').css('display','flex');
+    }
 }
+
+function viewLeader(e) {}
