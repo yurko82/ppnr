428eec3a37f6ff7f3c80460f75cca8b06217e0f6
spineag <spineag@gmail.com>
2021-01-20 21:08:52 +0200

    changes for rnpp_lightbox -part1

--- js/rnpp_lightbox.js
+++ js/rnpp_lightbox.js
@@ -15,12 +15,17 @@ class RnppLightbox {
             html+=`<div class="column pointer"><img src="${this.photos[i]}" style="width:100%" onclick="lightbox.openModal();lightbox.currentSlide(${parseInt(i+1)})" class="hover-shadow cursor"></div>`;
         }
         html+=`</div>  <div class="modal">
-        <button class="" onclick="lightbox.closeModal()"><a class="icon-close close cursor"></a></button>
-        <div class="caption-container"><p>Caption</p></div>
-        <div class="modal-content">`;
+        <button onclick="lightbox.closeModal()"><a class="icon-close close cursor"></a></button>
+        <div class="modal-content">
+            <div class="modal-arrow-container">
+                    <button class="btn-arrow btn-d btn-arrow-left pointer white" onclick="lightbox.plusSlides(-1)"><a class="icon-left-arrow"></a></button>
+                    <button class="btn-arrow btn-d btn-arrow-right pointer white" onclick="lightbox.plusSlides(1)"><a class="icon-right-arrow"></a></button>
+                </div>`;
         for (i=0;i<l;i++){
             html+=`<div class="bigphoto" draggable="false">
+                    <div class="caption-container"><span>${this.texts[i]}</span></div>
                     <img draggable="false" src="${this.photos[i]}" style="width:100%">
+                    <div class="numbertext-container"><span>${parseInt(i+1)} / ${parseInt(l)}</span></div> 
                 </div>`;
         }
         html+=`<div class="options"><div><span class="numbertext">0/0</span></div> 
@@ -37,7 +42,6 @@ class RnppLightbox {
         this.bigphotos=this.parent.find(".bigphoto").toArray();
         this.dots=this.parent.find(".halftransparent").toArray();
         this.modal=this.parent.find(".modal");
-        this.captionText = this.parent.find(".caption-container p");
         this.numberText = this.parent.find('.numbertext');
         this.bigphotos.forEach(el => {
             new RnppTouches(el, {swipeLeft: ()=>this.plusSlides(1), swipeRight: ()=>this.plusSlides(-1), touch:true, mouse:true});
@@ -66,7 +70,6 @@ class RnppLightbox {
         }
         this.bigphotos[this.slideIndex-1].style.display = "block";
         this.dots[this.slideIndex-1].className += " active";
-        this.captionText.text(this.texts[this.slideIndex-1] || ''); //this.dots[this.slideIndex-1].alt;
         this.numberText.text(n+' / '+this.bigphotos.length);
     }
 }
