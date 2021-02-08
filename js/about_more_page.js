2cdfe5858a803a0dd6ad2556ca187f42afa82db7
yurko82 <yurko1982@gmail.com>
2021-02-08 15:06:47 +0200

    edit career

--- js/about_more_page.js
+++ js/about_more_page.js
@@ -1,14 +1,14 @@
-    let arBlocks = $('.allabout__content .block').toArray();
-    $(".block-title").click((e)=>{
-        let bt, p=$(e.target).parent().toArray()[0];
+    let arBlocks = $('.block').toArray();
+    $(".block-title").click((e) => {
+        let bt, p = $(e.target).parent().toArray()[0];
         arBlocks.forEach(el => {
-            if (el==p){
+            if (el == p) {
                 $(e.target).toggleClass('show');
                 $(e.target).find('.angle-down').toggleClass('show');
                 $(p).find('.block-content').toggle(300);
             } else {
-                bt=$($(el).find('.block-title')[0]);
-                if (bt.hasClass('show')){
+                bt = $($(el).find('.block-title')[0]);
+                if (bt.hasClass('show')) {
                     bt.removeClass('show');
                     bt.find('.angle-down').removeClass('show');
                     $(el).find('.block-content').toggle(300);
@@ -17,7 +17,7 @@
         });
     });
 
-    let arURLs=[
+    let arURLs = [
         'https://www.rnpp.rv.ua/assets/images/czentr/poles/infocentr_2017_1.jpg',
         'https://www.rnpp.rv.ua/assets/images/czentr/poles/infocentr_2017_2.jpg',
         'https://www.rnpp.rv.ua/assets/images/czentr/poles/infocentr_2017_3.jpg',
@@ -29,6 +29,5 @@
         'https://www.rnpp.rv.ua/assets/images/czentr/poles/infocentr_2017_9.jpg',
         'https://www.rnpp.rv.ua/assets/images/czentr/poles/infocentr_2017_10.jpg'
     ];
-    let texts=['Опис 1', 'Опис 2', '', 'Опис 4', 'Опис 5', 'Опис 6', 'Опис 7', 'Опис 8', 'Опис 9', 'Опис 10'];
+    let texts = ['Опис 1', 'Опис 2', '', 'Опис 4', 'Опис 5', 'Опис 6', 'Опис 7', 'Опис 8', 'Опис 9', 'Опис 10'];
     let lightbox = new RnppLightbox('photo-gallery', arURLs, texts);
-
