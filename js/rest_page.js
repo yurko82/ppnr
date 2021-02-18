ac995dfba602c9cca11b040f06d901dde86ddbf7
yurko82 <yurko1982@gmail.com>
2021-02-18 07:02:50 +0200

    edit rest

---
+++ js/rest_page.js
@@ -0,0 +1,49 @@
+let arTip1 = [
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-05-31-12-49_house_type_1_1.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-05-31-12-49_house_type_1_2.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-05-31-12-49_house_type_1_3.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-05-31-12-49_house_type_1_4.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-05-31-12-49_house_type_1_5.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-05-31-12-49_house_type_1_6.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-08-09-10-07_house_type_I_1.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-08-09-10-07_house_type_I_2.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-08-09-10-07_house_type_I_3.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-08-09-10-07_house_type_I_4.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-08-09-10-07_house_type_I_5.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-08-09-10-07_house_type_I_6.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-08-09-10-07_house_type_I_7.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-08-09-10-07_house_type_I_8.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-08-09-10-07_house_type_I_9.JPG'
+];
+let arTip2 = [
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-05-31-13-30_house_type_2_1.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-05-31-13-30_house_type_2_2.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-05-31-13-30_house_type_2_3.JPG'
+];
+let arTip3 = [
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-05-31-13-34_house_type_3_1.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-05-31-13-34_house_type_3_2.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-05-31-13-34_house_type_3_3.JPG'
+];
+let arTip4 = [
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-05-31-13-38_house_type_4_1.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-05-31-13-38_house_type_4_2.JPG',
+    'https://www.rnpp.rv.ua/assets/images/objekti/ozero/2018-05-31-13-38_house_type_4_3.JPG'
+
+];
+let texts1 = ['Загальний вигляд ПТУ ВП РАЕС', 'Кабінет енергетичних дисциплін', 'Слюсарна майстерня', 'Майстерня зварювання', 'Деревообробна майстерня',
+    'Бібліотека', 'Кабінет загальноосвітніх дисциплін', 'Кабінет спецтехнологій зварників', 'Спортзал'
+];
+let texts2 = ['Загальний вигляд ПТУ ВП РАЕС', 'Кабінет енергетичних дисциплін', 'Слюсарна майстерня', 'Майстерня зварювання', 'Деревообробна майстерня',
+    'Бібліотека', 'Кабінет загальноосвітніх дисциплін', 'Кабінет спецтехнологій зварників', 'Спортзал'
+];
+let texts3 = ['Загальний вигляд ПТУ ВП РАЕС', 'Кабінет енергетичних дисциплін', 'Слюсарна майстерня', 'Майстерня зварювання', 'Деревообробна майстерня',
+    'Бібліотека', 'Кабінет загальноосвітніх дисциплін', 'Кабінет спецтехнологій зварників', 'Спортзал'
+];
+let texts4 = ['Загальний вигляд ПТУ ВП РАЕС', 'Кабінет енергетичних дисциплін', 'Слюсарна майстерня', 'Майстерня зварювання', 'Деревообробна майстерня',
+    'Бібліотека', 'Кабінет загальноосвітніх дисциплін', 'Кабінет спецтехнологій зварників', 'Спортзал'
+];
+let lightbox = new RnppLightbox('photo-gallery-1', arTip1, texts1);
+let lightbox2 = new RnppLightbox('photo-gallery-2', arTip2, texts2);
+let lightbox3 = new RnppLightbox('photo-gallery-3', arTip3, texts3);
+let lightbox4 = new RnppLightbox('photo-gallery-4', arTip4, texts4);
