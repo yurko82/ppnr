c5e54899b44efca52dfff2e734b34fc2820d1b04
yurko82 <yurko1982@gmail.com>
2021-03-19 12:55:28 +0200

    + region

--- js/rest_page.js
+++ js/rest_page.js
@@ -12,12 +12,12 @@ let arTipPreview1 = [
     '../images/rest/lakehouse_1_10_preview.jpg', '../images/rest/lakehouse_1_11_preview.jpg', '../images/rest/lakehouse_1_12_preview.jpg',
     '../images/rest/lakehouse_1_13_preview.jpg', '../images/rest/lakehouse_1_14_preview.jpg', '../images/rest/lakehouse_1_15_preview.jpg'
 ];
-let arTip2 = ['../images/rest/lakehouse_2_1.jpg', '../images/rest/lakehouse_2_2.jpg', '../images/rest/lakehouse_2_3.jpg',];
-let arTipPreview2 = ['../images/rest/lakehouse_2_1_preview.jpg', '../images/rest/lakehouse_2_2_preview.jpg', '../images/rest/lakehouse_2_3_preview.jpg',];
-let arTip3 = ['../images/rest/lakehouse_3_1.jpg', '../images/rest/lakehouse_3_2.jpg', '../images/rest/lakehouse_3_3.jpg',];
-let arTipPreview3 = ['../images/rest/lakehouse_3_1_preview.jpg', '../images/rest/lakehouse_3_2_preview.jpg', '../images/rest/lakehouse_3_3_preview.jpg',];
-let arTip4 = ['../images/rest/lakehouse_4_1.jpg', '../images/rest/lakehouse_4_2.jpg', '../images/rest/lakehouse_4_3.jpg',];
-let arTipPreview4 = ['../images/rest/lakehouse_4_1_preview.jpg', '../images/rest/lakehouse_4_2_preview.jpg', '../images/rest/lakehouse_4_3_preview.jpg',];
+let arTip2 = ['../images/rest/lakehouse_2_1.jpg', '../images/rest/lakehouse_2_2.jpg', '../images/rest/lakehouse_2_3.jpg', ];
+let arTipPreview2 = ['../images/rest/lakehouse_2_1_preview.jpg', '../images/rest/lakehouse_2_2_preview.jpg', '../images/rest/lakehouse_2_3_preview.jpg', ];
+let arTip3 = ['../images/rest/lakehouse_3_1.jpg', '../images/rest/lakehouse_3_2.jpg', '../images/rest/lakehouse_3_3.jpg', ];
+let arTipPreview3 = ['../images/rest/lakehouse_3_1_preview.jpg', '../images/rest/lakehouse_3_2_preview.jpg', '../images/rest/lakehouse_3_3_preview.jpg', ];
+let arTip4 = ['../images/rest/lakehouse_4_1.jpg', '../images/rest/lakehouse_4_2.jpg', '../images/rest/lakehouse_4_3.jpg', ];
+let arTipPreview4 = ['../images/rest/lakehouse_4_1_preview.jpg', '../images/rest/lakehouse_4_2_preview.jpg', '../images/rest/lakehouse_4_3_preview.jpg', ];
 
 let texts1 = ['Загальний вигляд ПТУ ВП РАЕС', 'Кабінет енергетичних дисциплін', 'Слюсарна майстерня', 'Майстерня зварювання', 'Деревообробна майстерня',
     'Бібліотека', 'Кабінет загальноосвітніх дисциплін', 'Кабінет спецтехнологій зварників', 'Спортзал'
@@ -36,19 +36,19 @@ let lightbox2 = new RnppLightbox('photo-gallery-2', arTipPreview2, arTip2, texts
 let lightbox3 = new RnppLightbox('photo-gallery-3', arTipPreview3, arTip3, texts3);
 let lightbox4 = new RnppLightbox('photo-gallery-4', arTipPreview4, arTip4, texts4);
 
-let arHotel=[
+let arHotel = [
     '../images/rest/hotel1.jpg', '../images/rest/hotel2.jpg', '../images/rest/hotel3.jpg', '../images/rest/hotel4.jpg', '../images/rest/hotel5.jpg',
     '../images/rest/hotel6.jpg', '../images/rest/hotel7.jpg', '../images/rest/hotel8.jpg', '../images/rest/hotel9.jpg', '../images/rest/hotel10.jpg',
     '../images/rest/hotel11.jpg', '../images/rest/hotel12.jpg', '../images/rest/hotel13.jpg', '../images/rest/hotel14.jpg', '../images/rest/hotel15.jpg'
 ];
-let arHotelPreview=[
+let arHotelPreview = [
     '../images/rest/hotel1-preview.jpg', '../images/rest/hotel2-preview.jpg', '../images/rest/hotel3-preview.jpg', '../images/rest/hotel4-preview.jpg',
-    '../images/rest/hotel5-preview.jpg','../images/rest/hotel6-preview.jpg', '../images/rest/hotel7-preview.jpg', '../images/rest/hotel8-preview.jpg',
+    '../images/rest/hotel5-preview.jpg', '../images/rest/hotel6-preview.jpg', '../images/rest/hotel7-preview.jpg', '../images/rest/hotel8-preview.jpg',
     '../images/rest/hotel9-preview.jpg', '../images/rest/hotel10-preview.jpg', '../images/rest/hotel11-preview.jpg',
     '../images/rest/hotel12-preview.jpg', '../images/rest/hotel13-preview.jpg', '../images/rest/hotel14-preview.jpg', '../images/rest/hotel15-preview.jpg'
 ];
-let textsHotel=[
-    'Загальний вигляд', 'Головний вхід','Зона очікування та відпочинку','Номер напівлюкс', 'Напівлюкс', 'Номер Люкс', 'Номер Люкс',
+let textsHotel = [
+    'Загальний вигляд', 'Головний вхід', 'Зона очікування та відпочинку', 'Номер напівлюкс', 'Напівлюкс', 'Номер Люкс', 'Номер Люкс',
     'Санвузол люкс', 'Одномісний номер', 'Одномісний номер', 'Санвузол одномісного номера', 'Двомісний номер'
 ];
-let lightboxHOtel = new RnppLightbox('photo-gallery-10', arHotelPreview, arHotel, textsHotel);
+let lightboxHotel = new RnppLightbox('photo-gallery-10', arHotelPreview, arHotel, textsHotel);
