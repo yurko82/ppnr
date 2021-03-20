00448d98150583238e2ffc471d55bb0d7b6f3e7f
yurko82 <yurko1982@gmail.com>
2021-03-20 08:49:45 +0200

    add sport

---
+++ js/sport_page.js
@@ -0,0 +1,54 @@
+let arTip1 = [
+    '../images/rest/lakehouse_1_1.jpg', '../images/rest/lakehouse_1_2.jpg', '../images/rest/lakehouse_1_3.jpg',
+    '../images/rest/lakehouse_1_4.jpg', '../images/rest/lakehouse_1_5.jpg', '../images/rest/lakehouse_1_6.jpg',
+    '../images/rest/lakehouse_1_7.jpg', '../images/rest/lakehouse_1_8.jpg', '../images/rest/lakehouse_1_9.jpg',
+    '../images/rest/lakehouse_1_10.jpg', '../images/rest/lakehouse_1_11.jpg', '../images/rest/lakehouse_1_12.jpg',
+    '../images/rest/lakehouse_1_13.jpg', '../images/rest/lakehouse_1_14.jpg', '../images/rest/lakehouse_1_15.jpg'
+];
+let arTipPreview1 = [
+    '../images/rest/lakehouse_1_1_preview.jpg', '../images/rest/lakehouse_1_2_preview.jpg', '../images/rest/lakehouse_1_3_preview.jpg',
+    '../images/rest/lakehouse_1_4_preview.jpg', '../images/rest/lakehouse_1_5_preview.jpg', '../images/rest/lakehouse_1_6_preview.jpg',
+    '../images/rest/lakehouse_1_7_preview.jpg', '../images/rest/lakehouse_1_8_preview.jpg', '../images/rest/lakehouse_1_9_preview.jpg',
+    '../images/rest/lakehouse_1_10_preview.jpg', '../images/rest/lakehouse_1_11_preview.jpg', '../images/rest/lakehouse_1_12_preview.jpg',
+    '../images/rest/lakehouse_1_13_preview.jpg', '../images/rest/lakehouse_1_14_preview.jpg', '../images/rest/lakehouse_1_15_preview.jpg'
+];
+let arTip2 = ['../images/rest/lakehouse_2_1.jpg', '../images/rest/lakehouse_2_2.jpg', '../images/rest/lakehouse_2_3.jpg', ];
+let arTipPreview2 = ['../images/rest/lakehouse_2_1_preview.jpg', '../images/rest/lakehouse_2_2_preview.jpg', '../images/rest/lakehouse_2_3_preview.jpg', ];
+let arTip3 = ['../images/rest/lakehouse_3_1.jpg', '../images/rest/lakehouse_3_2.jpg', '../images/rest/lakehouse_3_3.jpg', ];
+let arTipPreview3 = ['../images/rest/lakehouse_3_1_preview.jpg', '../images/rest/lakehouse_3_2_preview.jpg', '../images/rest/lakehouse_3_3_preview.jpg', ];
+let arTip4 = ['../images/rest/lakehouse_4_1.jpg', '../images/rest/lakehouse_4_2.jpg', '../images/rest/lakehouse_4_3.jpg', ];
+let arTipPreview4 = ['../images/rest/lakehouse_4_1_preview.jpg', '../images/rest/lakehouse_4_2_preview.jpg', '../images/rest/lakehouse_4_3_preview.jpg', ];
+
+let texts1 = ['Загальний вигляд ПТУ ВП РАЕС', 'Кабінет енергетичних дисциплін', 'Слюсарна майстерня', 'Майстерня зварювання', 'Деревообробна майстерня',
+    'Бібліотека', 'Кабінет загальноосвітніх дисциплін', 'Кабінет спецтехнологій зварників', 'Спортзал'
+];
+let texts2 = ['Загальний вигляд ПТУ ВП РАЕС', 'Кабінет енергетичних дисциплін', 'Слюсарна майстерня', 'Майстерня зварювання', 'Деревообробна майстерня',
+    'Бібліотека', 'Кабінет загальноосвітніх дисциплін', 'Кабінет спецтехнологій зварників', 'Спортзал'
+];
+let texts3 = ['Загальний вигляд ПТУ ВП РАЕС', 'Кабінет енергетичних дисциплін', 'Слюсарна майстерня', 'Майстерня зварювання', 'Деревообробна майстерня',
+    'Бібліотека', 'Кабінет загальноосвітніх дисциплін', 'Кабінет спецтехнологій зварників', 'Спортзал'
+];
+let texts4 = ['Загальний вигляд ПТУ ВП РАЕС', 'Кабінет енергетичних дисциплін', 'Слюсарна майстерня', 'Майстерня зварювання', 'Деревообробна майстерня',
+    'Бібліотека', 'Кабінет загальноосвітніх дисциплін', 'Кабінет спецтехнологій зварників', 'Спортзал'
+];
+let lightbox1 = new RnppLightbox('photo-gallery-1', arTipPreview1, arTip1, texts1);
+let lightbox2 = new RnppLightbox('photo-gallery-2', arTipPreview2, arTip2, texts2);
+let lightbox3 = new RnppLightbox('photo-gallery-3', arTipPreview3, arTip3, texts3);
+let lightbox4 = new RnppLightbox('photo-gallery-4', arTipPreview4, arTip4, texts4);
+
+let arHotel = [
+    '../images/rest/hotel1.jpg', '../images/rest/hotel2.jpg', '../images/rest/hotel3.jpg', '../images/rest/hotel4.jpg', '../images/rest/hotel5.jpg',
+    '../images/rest/hotel6.jpg', '../images/rest/hotel7.jpg', '../images/rest/hotel8.jpg', '../images/rest/hotel9.jpg', '../images/rest/hotel10.jpg',
+    '../images/rest/hotel11.jpg', '../images/rest/hotel12.jpg', '../images/rest/hotel13.jpg', '../images/rest/hotel14.jpg', '../images/rest/hotel15.jpg'
+];
+let arHotelPreview = [
+    '../images/rest/hotel1-preview.jpg', '../images/rest/hotel2-preview.jpg', '../images/rest/hotel3-preview.jpg', '../images/rest/hotel4-preview.jpg',
+    '../images/rest/hotel5-preview.jpg', '../images/rest/hotel6-preview.jpg', '../images/rest/hotel7-preview.jpg', '../images/rest/hotel8-preview.jpg',
+    '../images/rest/hotel9-preview.jpg', '../images/rest/hotel10-preview.jpg', '../images/rest/hotel11-preview.jpg',
+    '../images/rest/hotel12-preview.jpg', '../images/rest/hotel13-preview.jpg', '../images/rest/hotel14-preview.jpg', '../images/rest/hotel15-preview.jpg'
+];
+let textsHotel = [
+    'Загальний вигляд', 'Головний вхід', 'Зона очікування та відпочинку', 'Номер напівлюкс', 'Напівлюкс', 'Номер Люкс', 'Номер Люкс',
+    'Санвузол люкс', 'Одномісний номер', 'Одномісний номер', 'Санвузол одномісного номера', 'Двомісний номер'
+];
+let lightboxHotel = new RnppLightbox('photo-gallery-10', arHotelPreview, arHotel, textsHotel);
