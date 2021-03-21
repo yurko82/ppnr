6c2c155737d9be55414ab560d8b4bcba9f358f62
spineag <spineag@gmail.com>
2021-03-21 21:29:14 +0200

    page r5 -part2

---
+++ js/r5.js
@@ -0,0 +1,26 @@
+(function() {
+
+    let toTop=0, up=0;
+    $(window).resize(function() {
+        let _w = window.innerWidth;
+        if (_w>=1000) toTop=200;
+        else if (_w>768) toTop=114;
+        else toTop=100;
+        up=0;
+        $(window).triggerHandler('scroll');
+    });
+
+    $(window).on("scroll", function() {
+        if ($(window).scrollTop()>=toTop){
+            if (up==1) return;
+            $('.container_r5 .big_text_cont').removeClass('big_text_cont-fixed');
+            up=1;
+        } else{
+            if (up==2) return;
+            $('.container_r5 .big_text_cont').addClass('big_text_cont-fixed');
+            up=2;
+        }
+    });
+
+    $(window).triggerHandler('resize');
+})();
