ffca6d7d8b8124262c2688e4327c5d4a01be0015
yurko82 <yurko1982@gmail.com>
2021-03-24 13:27:04 +0200

    info block add radioactive

--- js/index__info-block.js
+++ js/index__info-block.js
@@ -1,6 +1,10 @@
 (function() {
 
-    let current = 0, screenW=0, pictureW, isAnimate=false, isFade, container = $('section.info'), arrayInfo=[];
+    let current = 0,
+        screenW = 0,
+        pictureW, isAnimate = false,
+        isFade, container = $('section.info'),
+        arrayInfo = [];
 
     let data = [{
         type: "baner",
@@ -26,7 +30,7 @@
         url: 'html/some_page3.html',
         use: true
     }];
-    
+
 
     function _checkArrayInfo(w) {
         arrayInfo = data.filter(x => x.use && (w >= 768 || x.type != 'data')).sort((a, b) => a.priority > b.priority ? 1 : -1);
@@ -47,31 +51,32 @@
         else if (w > 700) t = 1000;
         else if (w >= 500) t = 650;
         else t = 400;
-        if (screenW  == 0 || t != pictureW || w < 768 && screenW >= 768 || w >= 768 && screenW < 768) {
+        if (screenW == 0 || t != pictureW || w < 768 && screenW >= 768 || w >= 768 && screenW < 768) {
             pictureW = t;
-            if (screenW  == 0 || w < 768 && screenW >= 768 || w >= 768 && screenW < 768) _checkArrayInfo(w);
+            if (screenW == 0 || w < 768 && screenW >= 768 || w >= 768 && screenW < 768) _checkArrayInfo(w);
             _update();
         }
         screenW = w;
     }
 
-    function _update(){
+    function _update() {
         if (!arrayInfo.length) {
             container.css('display', 'none');
             return;
         }
 
-        container.css('display','grid');
-        current=0;
+        container.css('display', 'grid');
+        current = 0;
         document.querySelector('section.info .info__block .title span').innerText = arrayInfo[0].title;
 
-        let i, html='', countImg=0;
+        let i, html = '',
+            countImg = 0;
         for (let i = 0; i < arrayInfo.length; i++) {
             html += `<div class="info__parallax-item" style="background-image: url('${arrayInfo[i].bg}');"></div>`;
         }
         document.querySelector('section.info #info-bg-slider').innerHTML = html;
 
-        html='<div id="info-items-slider">';
+        html = '<div id="info-items-slider">';
         for (i = 0; i < arrayInfo.length; i++) {
             if (arrayInfo[i].type == "baner") {
                 html += `<img class="info__item" src="${arrayInfo[i].pics[pictureW]||arrayInfo[i].pics['1280']}" alt="">`;
@@ -148,17 +153,28 @@
                                 <span class="pokaz">46</span>
                                 <span class="odin">%</span>
                             </div>
+                            <span class="info__statistic_title">Радіаційний стан</span>
+                            <div class="d-flex justify-content-between align-items-baseline">
+                                <span class="text flex-grow-1">Пром. майданчик</span>
+                                <span class="pokaz">0.09</span>
+                                <span class="odin">мкЗв/г</span>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-baseline">
+                                <span class="text flex-grow-1">м. Вараш</span>
+                                <span class="pokaz">0.08</span>
+                                <span class="odin">мкЗв/г</span>
+                            </div>
                         </div>
                     </div>
                 </div>`;
             }
         }
-        html+='</div>';
+        html += '</div>';
         document.querySelector('section.info #info-items-slider_container').innerHTML = html;
         isFade = screenW <= 1000;
-        
-        if (arrayInfo.length>1) {
-            document.querySelector('section.info .btn-arrow-container').style.display ='flex';
+
+        if (arrayInfo.length > 1) {
+            document.querySelector('section.info .btn-arrow-container').style.display = 'flex';
             if (countImg > 0) {
                 __onLoadImg = function() {
                     countImg--;
@@ -179,13 +195,13 @@
                             accessibility: false
                         });
                         $('section.info #info-items-slider').on('afterChange', () => isAnimate = false);
-                        $('section.info .slick-list.draggable').css('height','100%');
+                        $('section.info .slick-list.draggable').css('height', '100%');
                         _btn();
                     }
                 }
                 Array.prototype.forEach.call($('section.info #info-items-slider img.info__item'), el => { if (el.src) el.onload = () => __onLoadImg(); });
             }
-        } else document.querySelector('section.info .btn-arrow-container').style.display ='none';
+        } else document.querySelector('section.info .btn-arrow-container').style.display = 'none';
     }
 
     function _moveSlider(right) {
@@ -195,20 +211,20 @@
             $('section.info #info-bg-slider').slick('slickNext');
             $('section.info #info-items-slider').slick('slickNext');
             current++;
-            if (current>=arrayInfo.length) current=0;
+            if (current >= arrayInfo.length) current = 0;
         } else {
             $('section.info #info-bg-slider').slick('slickPrev');
             $('section.info #info-items-slider').slick('slickPrev');
             current--;
-            if (current<0) current=arrayInfo.length-1;
+            if (current < 0) current = arrayInfo.length - 1;
         }
         _changeTitle(arrayInfo[current].title);
         _btn();
     }
 
-    function _btn(){
-        container.find('button.info__btn').on('click', ()=>{
-            window.location = window.location.href.replace('index.html','') + arrayInfo[current].url;
+    function _btn() {
+        container.find('button.info__btn').on('click', () => {
+            window.location = window.location.href.replace('index.html', '') + arrayInfo[current].url;
             return false;
         });
     }
