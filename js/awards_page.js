f9152c09eac5b1e790f945818b10097e12e506c9
spineag2 <spineag@gmail.com>
2021-04-19 11:57:23 +0300

    adds to awards page

--- js/awards_page.js
+++ js/awards_page.js
@@ -1,12 +1,12 @@
 // about_awards.html
 $(document).ready(function() {
-
-    $(".awards li").hover(function() {
+    $(".awards li").click(function() {
+        if ($(window).width()<1000) return;
         let liId = $(this).attr('id');
-        console.log(liId);
         $(".awards__view img").attr('src', '../images/award/' + liId + '.jpg');
     });
 });
+
 let arNtc = [
     '../images/award/award_1.jpg', '../images/award/award_2.jpg', '../images/award/award_3.jpg',
     '../images/award/award_4.jpg', '../images/award/award_5.jpg', '../images/award/award_6.jpg',
@@ -14,10 +14,7 @@ let arNtc = [
     '../images/award/award_10.jpg', '../images/award/award_11.jpg', '../images/award/award_12.jpg',
     '../images/award/award_13.jpg', '../images/award/award_14.jpg', '../images/award/award_15.jpg'
 ];
-
-
 let textsNtc = ['Загальний вигляд ПТУ ВП РАЕС', 'Кабінет енергетичних дисциплін', 'Слюсарна майстерня', 'Майстерня зварювання', 'Деревообробна майстерня',
     'Бібліотека', 'Кабінет загальноосвітніх дисциплін', 'Кабінет спецтехнологій зварників', 'Спортзал'
 ];
-
 let lightboxAwards = new RnppLightbox('photo-gallery-awards', arNtc, arNtc, textsNtc);
