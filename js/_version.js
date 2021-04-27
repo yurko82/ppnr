c0f08ce334df4668912dbd3a0aa4d9db16eb425c
spineag2 <spineag@gmail.com>
2021-04-27 11:58:57 +0300

    update versions

--- js/_version.js
+++ js/_version.js
@@ -1,31 +1,47 @@
 let _v = (function(){
     let _queue=[];
     let versionJS = {
-        'main':4,
-        'index_page':3,
-        'index__head-slider-block':2,
-        'index__info-block':2,
-        'index__news-block':2,
-        'leaders_page':4,
-        'news_page':3,
-        'about_menu':2,
-        'about_more_page':1,
-        'article_page':1,
-        'history_additional':1,
+        'main':5,
+        'index_page':4,
+        'index__head-slider-block':3,
+        'index__info-block':3,
+        'index__news-block':3,
+        'leaders_page':5,
+        'news_page':4,
+        'about_menu':3,
+        'about_more_page':2,
+        'article_page':2,
+        'history_additional':2,
         'rnpp_lightbox':2,
-        'rnpp_touches':1,
-        'text_blocks':1
+        'rnpp_touches':2,
+        'text_blocks':2,
+        'about_activity_page':2,
+        'allnews':2,
+        'awards_page':2,
+        'career_page':2,
+        'culture_page':2,
+        'employee_page':2,
+        'leaders_page':2,
+        'new_page_single':2,
+        'r5':2,
+        'region_page':2,
+        'rest_page':2,
+        'rnpp_anchors':2,
+        'rnpp_content_hidder':2,
+        'rnpp_infographics':2,
+        'sport_page':2,
+        'study_page':2
     };
 
     let versionCSS = {
-        'rnpp-icons':3,
-        'page_index':7,
-        'page_news':7,
-        'page_allnews':7,
-        'page_about':8,
+        'rnpp-icons':4,
+        'page_index':9,
+        'page_news':9,
+        'page_allnews':9,
+        'page_about':9,
         'page_about_history':10,
-        'page_about_leader':8,
-        'common':3
+        'page_about_leader':9,
+        'common':4
     }
 
     let getJSversion=function(filename){ return versionJS[filename]||''; }
